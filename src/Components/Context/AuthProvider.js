225761a6fe694cd929894723470876d0a92c6eb8
Sagar Khandelwal <sagarcom853@gmail.com>
2025-08-01 19:17:51 +0530

    Add session persistence to the login

--- src/Components/Context/AuthProvider.js
+++ src/Components/Context/AuthProvider.js
@@ -1,30 +1,48 @@
-import React, { createContext, useState, useContext } from 'react'
+import React, { createContext, useState, useContext } from "react";
 
-const AuthContext = createContext(null)
+const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({ isAuthenticated: false, token: "" , user: {}})
-    const [theme, setTheme] = useState("light")
-    const login = (user, token='') => {
-        setAuth({ ...auth, isAuthenticated: true, token: token , user : user})
-    }
-    const logout = () => {
-        setAuth({ ...auth, isAuthenticated: false, token: "", user: {}})
-    }
-    // Function to toggle the theme
-    const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  const initialAuthState = {
+    isAuthenticated: false,
+    token: "",
+    user: {},
+  };
+  // Retrieve user info from localStorage or set to initial state
+  let userInfo = JSON.parse(localStorage.getItem("auth")) || {};
+  const [auth, setAuth] = useState(userInfo || initialAuthState);
+  const [theme, setTheme] = useState("light");
+  const login = (user, token = "") => {
+    let authObject = {
+      ...auth,
+      isAuthenticated: true,
+      token: token,
+      user: user,
     };
+    localStorage.setItem("auth", JSON.stringify(authObject));
+    setAuth(authObject);
+  };
+  const logout = () => {
+    let authObject = { ...auth, isAuthenticated: false, token: "", user: {} };
+    localStorage.setItem("auth", JSON.stringify(authObject));
+    setAuth(authObject);
+  };
+  // Function to toggle the theme
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
-    return (
-        <AuthContext.Provider value={{ login, logout, ...auth, setAuth, toggleTheme, theme, }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{ login, logout, ...auth, setAuth, toggleTheme, theme }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 const useAuth = () => {
-    return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
 
-export { useAuth }
-export default AuthProvider
+export { useAuth };
+export default AuthProvider;
